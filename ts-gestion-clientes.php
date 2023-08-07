d4713a232e1021e1658c36b0c62ca789699f696a
Sergio D. Talavera <sergiodemiantalavera@gmail.com>
2023-08-07 19:32:07 -0300

    Esquema inicial de archivos

---
+++ ts-gestion-clientes.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name:       TS Gestión de Clientes
+ * Plugin URI:        https://github.com/talaverasergio/ts-gestion-clientes
+ * Description:       Permite la gestion de clientes de un negocio, tales como asistencias y control de pagos.
+ * Version:           0.1
+ * Author:            Sergio D. Talavera
+ * Author URI:        https://github.com/talaverasergio
+ * License:           GPLv3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
